36171f41ed256a02e9171eee80c4530faa06462f
Rajat Surana <rasurana19@gmail.com>
2026-05-01 11:16:14 +0530

    Updated with all backend

---
+++ backend/main.py
@@ -0,0 +1,27 @@
+import logging
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+
+from fastapi.middleware.cors import CORSMiddleware
+from database import create_tables
+from routes import router
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    create_tables()
+    yield
+
+
+app = FastAPI(
+    title="Youtube Thumbnail Maker",
+    lifespan=lifespan
+)
+
+app.middleware(
+    CORSMiddleware,
+    allow_origins = ["http://localhost:5173"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router)
